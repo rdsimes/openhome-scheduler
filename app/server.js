52619f1ab6af04978b067e2671d68b3d152006fd
Richard Simes <ricardo.nz@gmail.com>
2014-09-16 09:34:03 +1200

    set up handlebars as the view engine

--- app/server.js
+++ app/server.js
@@ -1,11 +1,14 @@
-var express = require('express');
+var express = require('express'),
+ exphbs  = require('express-handlebars');
+
 var app = express();
 
 app.set('views', './views');
 app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.render('index');
 });
 
 var server = app.listen(3000, function() {
